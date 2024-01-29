9d09051f8b5055c8e28c69833295516e2f0f38ac
sergio1024 <137495954+sergio1024@users.noreply.github.com>
2024-01-29 16:27:33 +0000

    Change 01

--- V-map/V-map.cpp
+++ V-map/V-map.cpp
@@ -1,21 +1,7 @@
-//_______________________________________________________________
-//_______________________________________________________________
-//In this solution I used Visual Studio 2022 to compile
-//______________________________________________________________
-//With this solution we can implement the values of each key value pair,
-//view all the values,
-//filter the value that corresponds to each key,
-//verify that all values are different, and
-//make comparisons between values.
-//_______________________________________________________________
-//_______________________________________________________________
-
 
 #include <iostream>
 #include <string>
 #include <map>
-
-char Valf = 10; //Key from which we want to get the value on the filter function
 
 template<typename K, typename V>
 
@@ -77,31 +63,64 @@ public:
 
 //Creates a range map, assigns values to a range of keys, and prints all key-value pairs
 void IntervalMapTest() {
-
 	interval_map<int, char> imap(' ');
 	imap.assign(0, 26, 'A');
-	std::cout << "All value pairs values:\n";
 
-	for (int i = 0; i < 26; i++) {
-		std::cout << i << " => " << imap[i] << '\n';
+	std::string userInput;
+	std::cout << "1 - Type 'c' to print all value pairs:\n";
+	std::getline(std::cin, userInput);
+
+	if (userInput == "c") {
+		std::cout << "All value pairs:\n\n";
+		for (int i = 0; i < 26; i++) {
+			std::cout << i << " => " << imap[i] << '\n';
+		}
 	}
-
+	else {
+		std::cout << "You have chosen not print the values.\n\n";
+	}
 }
+
+
 
 //Creates a range map, assigns values to a range of keys, and prints the value associated with a specific key
 static void Filter() {
 	interval_map<int, char> imap(' ');
 	imap.assign(0, 26, 'A');
-	char value = imap[Valf];
-	std::cout << "\nThe result of the Filter function is as follows: \nThe value associated with key " + std::to_string(Valf) + " is: " << value << '\n'; // printing the value on the console
+
+	int key;
+	bool validInput = false;
+
+	while (!validInput) {
+		std::cout << "3 - Enter now a value between 0 to 25: ";
+		std::cin >> key;
+
+		if (std::cin.fail()) {
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // descarta a entrada incorreta
+			std::cout << "Invalid input! Please enter a number between 0 and 25.\n\n";
+		}
+		else if (key < 0 || key > 25) {
+			std::cout << "Invalid key! Please enter a key between 0 and 25.\n\n";
+		}
+		else {
+			validInput = true;
+		}
+	}
+
+	char value = imap[key];
+	std::cout << "\n\nThe result of the Filter function is as follows: \nThe value associated with key " + std::to_string(key) + " is: " << value << '\n'; // printing the value on the console
 }
+
+
+
 
 //Checks to see if there are any key pairs on the map that have the same value associated with them
 static void CanonicalCheck() {
 	interval_map<int, char> imap(' ');
 	imap.assign(0, 26, 'A');
 
-	std::cout << "\nThe result of the CanonicalCheck fuction is:";
+	std::cout << "\nThe result of the Canonical Check fuction is:";
 	for (auto it1 = imap.begin(); it1 != imap.end(); ++it1) {
 		for (auto it2 = std::next(it1); it2 != imap.end(); ++it2) {
 			if (it1->second == it2->second) {
@@ -110,43 +129,72 @@ static void CanonicalCheck() {
 			}
 		}
 	}
-	std::cout << " \nAll key value pair values are different." << std::endl;
+	std::cout << " \nAll key value pair values are different.\n" << std::endl;
 }
 
 //Check if the value associated with key 5 and 10 are the same, the value of the keys can be changed freely,
-//2 global variables can also be created to make the comparison. It makes it easier to access the code.
-//example :
-//char a = 5;
-//char b = 10;
-//And then replace:
-//char val1 = imap[5];
-//char val2 = imap[10];
-//by;
-//char val1 = imap[a];
-//char val2 = imap[b];
 static void Conditionals() {
 	interval_map<int, char> imap(' ');
 	imap.assign(0, 26, 'A');
 
-	char val1 = imap[5];
-	char val2 = imap[10];
+	int key1, key2;
+
+	while (true) {
+		std::cout << "2 - Enter the first key value between 0 and 25:\n ";
+		if (!(std::cin >> key1) || key1 < 0 || key1 > 25) {
+			std::cout << "Invalid input! Please enter a number between 0 and 25.\n\n";
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+		else {
+			break;
+		}
+	}
+
+	while (true) {
+		std::cout << "2 - Enter the second key value between 0 and 25: ";
+		if (!(std::cin >> key2) || key2 < 0 || key2 > 25 || key2 == key1) {
+			std::cout << "Invalid input! Please enter a number between 0 and 25 that is different from the first key.\n\n";
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+		else {
+			break;
+		}
+	}
+
+	char val1 = imap[key1];
+	char val2 = imap[key2];
 
 	std::cout << "\nThe result of the Conditionals function is:";
 	if (val1 == val2) {
-		std::cout << "\nThe values associated with keys 5 and 10 are the same.\n";
+		std::cout << "\nThe values associated with keys " << key1 << " and " << key2 << " are the same.\n";
 	}
 	else {
-		std::cout << "\nThe values associated with keys 5 and 10 are not the same.\n";
+		std::cout << "\nThe values associated with keys " << key1 << " and " << key2 << " are not the same.\n";
 	}
 }
+
+
 
 
 int main()
 {
-	IntervalMapTest();
-	Filter();
-	CanonicalCheck();
+	std::cout << "Welcome!\n\n"
+		"This is a map range that automatically assigns values to a range of keys and their respective pairs.\n"
+		"The key value pair values are automatically assigned, and correspond to the letters of the alphabet\n\n"
+		"Here you can check the content and take the test under 2 conditions:\n\n"
+		"1 - Check the content\n"
+		"2 - Are the values canonical? In other words, are they consecutive and non-repeating values?\n"
+		"here the values of 2 keys will be compared, and it will be checked if all values are canonical\n"
+		"3 - Displays the associated value of any key between 0 and 25\n\n"
+		"Let's go:\n\n";
+
+	IntervalMapTest();	
 	Conditionals();
+	CanonicalCheck();
+	Filter();
 }
 
+// Sergio Arantes
 
